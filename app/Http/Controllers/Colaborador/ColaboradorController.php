b2b98baa8c5901544d2628de8e706b5c08dbc53b
Illian Yamandú <yamandu.developer@gmail.com>
2022-11-26 20:08:09 -0300

    preparando ambiente

---
+++ app/Http/Controllers/Colaborador/ColaboradorController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Colaborador;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ColaboradorController extends Controller
+{
+    //
+}
--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['prefix' => '/'], function () {
+    Route::get('', function () {
+        return view('dashboards.index');
+    });
 });
-Route::get('/admin', function () {
-    return view('admin');
-})->name('admin.dash');
