89eb17c02a5a814b4789d2f6126501845266a2d5
Illian Yamandú <yamandu.developer@gmail.com>
2022-12-04 20:28:57 -0300

    datatables

--- app/Models/Cliente.php
+++ app/Models/Cliente.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Cliente extends Model
 {
     use HasFactory;
+    
+    protected $table = "clientes";
 
     protected $fillable = ['nome', 'cpf', 'status', 'rg', 'endereco', 'email', 'telefone', 'instagram', 'data_nascimento'];
     protected $dates = ['data_nascimento', 'created_at', 'updated_at'];
